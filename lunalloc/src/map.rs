78eab5bf46b28432e0148ac8bd374e03048e72ed
Shark <sharktheone@proton.me>
2026-02-17 17:03:40 +0100

    add malloc fallback

--- lunalloc/src/map.rs
+++ lunalloc/src/map.rs
@@ -14,6 +14,18 @@ pub fn map(size: usize) -> Option<*mut u8> {
     {
         posix::map(size)
     }
+
+    #[cfg(not(any(target_family = "windows", target_family = "unix")))]
+    unsafe {
+        let ptr = malloc(size);
+
+        if ptr.is_null() {
+            // core::hint::cold_path();
+            None
+        } else {
+            Some(ptr as *mut u8)
+        }
+    }
 }
 
 pub fn unmap(ptr: *mut u8, size: usize) -> bool {
@@ -24,5 +36,16 @@ pub fn unmap(ptr: *mut u8, size: usize) -> bool {
     #[cfg(target_family = "unix")]
     {
         posix::unmap(ptr, size)
+    }
+
+    #[cfg(not(any(target_family = "windows", target_family = "unix")))]
+    {
+        if ptr.is_null() {
+            // core::hint::cold_path();
+            return false;
+        }
+
+        unsafe { libc::free(ptr as *mut _) }
+        true
     }
 }
