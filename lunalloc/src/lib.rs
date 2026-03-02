1c5ead8ed3ba6b5f0af1187fec1aeee442b14938
Shark <sharktheone@proton.me>
2026-03-03 00:35:55 +0100

    OS abstractions

--- lunalloc/src/lib.rs
+++ lunalloc/src/lib.rs
@@ -2,6 +2,7 @@
 #[cfg(not(any(windows, unix)))]
 extern crate alloc;
 
+#[cfg(any(windows, unix))]
 pub mod os;
 mod page;
 mod page_size;
--- lunalloc/src/os.rs
+++ lunalloc/src/os.rs
@@ -10,25 +10,66 @@ mod posix;
 
 
 pub(crate) unsafe fn reserve(size: NonZeroUsize) -> Option<NonNull<u8>> {
-    todo!()
+    #[cfg(windows)]
+    unsafe {
+        windows::reserve(size)
+    }
+    #[cfg(unix)]
+    unsafe {
+        posix::reserve(size)
+    }
 }
 
 pub(crate) unsafe fn commit(ptr: NonNull<u8>, size: NonZeroUsize) -> bool {
-    todo!()
+    #[cfg(windows)]
+    unsafe {
+        windows::commit(ptr, size)
+    }
+    #[cfg(unix)]
+    unsafe {
+        posix::commit(ptr, size)
+    }
 }
 
 pub(crate) unsafe fn decommit(ptr: NonNull<u8>, size: NonZeroUsize) -> bool {
-    todo!()
+    #[cfg(windows)]
+    unsafe {
+        windows::decommit(ptr, size)
+    }
+    #[cfg(unix)]
+    unsafe {
+        posix::decommit(ptr, size)
+    }
 }
-
-pub(crate) unsafe fn release(ptr: NonNull<u8>) -> bool {
-    todo!()
+pub(crate) unsafe fn release(ptr: NonNull<u8>, #[cfg(unix)] size: NonZeroUsize) -> bool {
+    #[cfg(windows)]
+    unsafe {
+        windows::release(ptr)
+    }
+    #[cfg(unix)]
+    unsafe {
+        posix::release(ptr, size)
+    }
 }
 
 pub(crate) unsafe fn alloc(size: NonZeroUsize) -> Option<NonNull<u8>> {
-    todo!()
+    #[cfg(windows)]
+    unsafe {
+        windows::alloc(size)
+    }
+    #[cfg(unix)]
+    unsafe {
+        posix::alloc(size)
+    }
 }
 
-pub(crate) unsafe fn free(ptr: NonNull<u8>) -> bool {
-    todo!()
+pub(crate) unsafe fn free(ptr: NonNull<u8>, #[cfg(unix)] size: NonZeroUsize) -> bool {
+    #[cfg(windows)]
+    unsafe {
+        windows::free(ptr)
+    }
+    #[cfg(unix)]
+    unsafe {
+        posix::free(ptr, size)
+    }
 }
